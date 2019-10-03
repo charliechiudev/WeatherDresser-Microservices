a1de89ad8e47e20a6a3d280395679132d628ec73
Charlie Chiu <41720382+charcharliepie@users.noreply.github.com>
2019-10-03 17:49:14 +1000

    custom tomorrow json list and random num() to get 1 set of dress

--- services/weather.service.js
+++ services/weather.service.js
@@ -31,7 +31,7 @@ module.exports = {
 
 				// build string with the url of openweathermap
 				var openweathermapApiKey = '931dd482db9863c889f68196f104be72';
-				var url = `https://api.openweathermap.org/data/2.5/weather?APPID=${openweathermapApiKey}&lat=${req.params.lat}&lon=${req.params.lat}`;
+				var url = `https://api.openweathermap.org/data/2.5/weather?APPID=${openweathermapApiKey}&lat=${req.params.lat}&lon=${req.params.lon}`;
 				
 				// call the openweathermap api
 				request(url, function (error, response, body) {
@@ -70,7 +70,7 @@ module.exports = {
 
 				// build string with the url of openweathermap
 				var openweathermapApiKey = '931dd482db9863c889f68196f104be72';
-				var url = `https://api.openweathermap.org/data/2.5/forecast?APPID=${openweathermapApiKey}&lat=${req.params.lat}&lon=${req.params.lat}`;
+				var url = `https://api.openweathermap.org/data/2.5/forecast?APPID=${openweathermapApiKey}&lat=${req.params.lat}&lon=${req.params.lon}`;
 				
 				// call the openweathermap api
 				request(url, function (error, response, body) {
@@ -82,9 +82,41 @@ module.exports = {
 
 					// the api contains the next 5 days, with array every three hours
 					var fiveDays = JSON.parse(body);
+					var cod = fiveDays.cod;
+					var t1 = fiveDays.list[7]["main"]["temp"];
+					var t2 = fiveDays.list[10]["main"]["temp"];
+					var temp = fiveDays.list[10]["main"]["temp"];
+					var main = fiveDays.list[10]["weather"][0]["main"];
+					var description = fiveDays.list[10]["weather"][0]["description"];
+					var icon = fiveDays.list[10]["weather"][0]["icon"];
+					var temp_min, temp_max;
+					if(t1 <= t2)
+						temp_min = t1;
+					else
+						temp_min = t2;
 
+					if(t1 > t2)
+						temp_max = t1;
+					else
+						temp_max = t2;
+
+					var tomorrow = {
+					"cod": cod,
+						"main":{
+							"temp_min": temp_min,
+							"temp_max": temp_max,
+							"temp": temp, 
+						},
+						"weather": [
+							{
+								"main": main,
+								"description": description,
+								"icon": icon
+							}
+						]
+					}
 					// for tomorrow, I'll return the segment of now + 24 hours ( 24 / 3 = 8, array start from 0, so the object number 7 is the one to return )
-					resolve(fiveDays.list[7]);
+					resolve(tomorrow);
 
 				});
 			});			
@@ -160,8 +192,42 @@ module.exports = {
 					// the api contains the next 5 days, with array every three hours
 					var fiveDays = JSON.parse(body);
 
+					// get useful items from list and store into custom 'tomorrow' which has exact same format as the 'now' one
+					var cod = fiveDays.cod;
+					var t1 = fiveDays.list[7]["main"]["temp"];
+					var t2 = fiveDays.list[10]["main"]["temp"];
+					var temp = fiveDays.list[10]["main"]["temp"];
+					var main = fiveDays.list[10]["weather"][0]["main"];
+					var description = fiveDays.list[10]["weather"][0]["description"];
+					var icon = fiveDays.list[10]["weather"][0]["icon"];
+					var temp_min, temp_max;
+					if(t1 <= t2)
+						temp_min = t1;
+					else
+						temp_min = t2;
+
+					if(t1 > t2)
+						temp_max = t1;
+					else
+						temp_max = t2;
+
+					var tomorrow = {
+					"cod": cod,
+						"main":{
+							"temp_min": temp_min,
+							"temp_max": temp_max,
+							"temp": temp, 
+						},
+						"weather": [
+							{
+								"main": main,
+								"description": description,
+								"icon": icon
+							}
+						]
+					}
 					// for tomorrow, I'll return the segment of now + 24 hours ( 24 / 3 = 8, array start from 0, so the object number 7 is the one to return )
-					resolve(fiveDays.list[7]);
+					resolve(tomorrow);
 
 				});
 			});			
